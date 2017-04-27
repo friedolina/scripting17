482a12c571bcab8c289ebb2faf5e7d4c0922b416
Frieda <scripting@hemio.de>
2017-04-27 13:56:08 +0000

    joa naja noch nicht ganz...

--- TCM/db_fuettern.php
+++ TCM/db_fuettern.php
@@ -4,24 +4,42 @@
         <title>TCM-Transformation</title>
         <meta charset="UTF-8">
         </head>
-        <body>
-            <?php
-                include 'connect.php';
-                include 'TCM.php';
+    <body>
+        <?php
+            include 'connect.php';
+            include 'TCM.php';
             
-               
-                $TCM = new SimpleXMLElement($xmlstr);
-                foreach ($TCM->kategorien->kategorie as $kategorie) {
-                    $insert_parts[] = $kategorie->name;
-            
-                    echo "<br>" . "Kategorie: " . $kategorie->name;
+            $TCM = new SimpleXMLElement($xmlstr);
+            $i = 0;
+            foreach ($TCM->kategorien->kategorie as $kategorie) {
+                $insert_parts[] = "(".$i.",'".$kategorie->name."')";
+                $i++;
+            }
+            $kat_query="INSERT INTO kategorie (kat_id, name) VALUES ".implode(",",$insert_parts).";";
+                 
+            //echo "<br>".$kat_query;     
+            mysqli_query($db, $kat_query) or die(mysqli_error($db));
+                
+            $i = 0;
+            foreach ($TCM->kraeuter->kraut as $kraut) {
+                
+                $wirkungs_array = array();
+                foreach ($kraut->wirkung as $wirkung) {
+                    $wirkungs_array[] = $wirkung;
                 }
-                $query="INSERT INTO kategorie (name) VALUES ('".implode("'),('",$insert_parts)."');";
-               echo $query;
-               mysqli_query($db, $query);
-                    /**$kat_eintrag = "INSERT INTO kategorie (name) VALUES ('$kategorie->name')";
-                    $eintragen = mysqli_query($db, $kat_eintrag);**/    
-            ?>
+                $merkmal_array = array();
+                foreach ($kraut->merkmal as $merkmal) {
+                    $merkmal_array[] = $merkmal;
+                }
+                $tuple_array[] = "(".$i.",'".$kraut->name."','".implode("--",$wirkungs_array)."','".implode("--",$merkmal_array)."','".$kraut->bild['quelle']."')";
+                $i++;
+            }
+            $kra_query = "INSERT INTO kraut (kra_id, name, wirkung, merkmal, bild) VALUES ".implode(",",$tuple_array).";";
+            //echo "<br>".$kra_query;
             
-            </body>
-            </html>
+            mysqli_query($db, $kra_query) or die(mysqli_error($db));
+            
+        ?>
+            
+    </body>
+</html>
